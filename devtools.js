4143b13323bb4f1982222b84c3e6f2a043c78042
Seth Silesky <seth.silesky@gmail.com>
2016-07-17 19:44:41 -0500

    "Parsing error: The keyword 'const' is reserved"

--- devtools.js
+++ devtools.js
@@ -1,6 +1,5 @@
 require(['eslint'], function (eslint) {
-    // foo is now loaded.
-
+    // foo is now loaded. //
 function debounce(func, wait, immediate) {
     var timeout;
     return function() {
@@ -20,7 +19,7 @@ function debounce(func, wait, immediate) {
 var verify = debounce(function() {
         var content = editor.getModel().getText();
         // this is what's up
-        var results = eslint.verify(content, OPTIONS);
+        var results = eslint.verify(content);
         console.log(results);
     }, 500);
 
@@ -43,14 +42,14 @@ function getOptions() {
 //   if (!isValid) {
 //     JSHINT.errors.forEach(function(error) {
 //       if (error === null) {
-//         // Why does JSHINT return a null terminated array?
+//         //  hy does JSHINT return a null terminated array?
 //         return;
 //       }
 //       chrome.experimental.devtools.console.addMessage(chrome.experimental.devtools.console.Severity.Error, error.reason, url, error.line);
 //     });
 //   } else {
 //     chrome.experimental.devtools.console.addMessage(chrome.experimental.devtools.console.Severity.Log, "JSHint: No errors", url);
-//   }
+//  /  /    }
 // }
 
 
@@ -73,21 +72,20 @@ var config = {
 };
 
 
-/* */
-function validate(content) {
-  var obj = eslint.verify(content, config); //make this object.reas
-
-     chrome.experimental.devtools.console.addMessage(chrome.experimental.devtools.console.Severity.Error, obj.reason, url, error.line);
+function validate(content, url) {
+  console.log(content);
+  const errorObj = eslint.verify(content, config);
+  console.log(errorObj); 
+//  chrome.experimental.devtools.consol e.addMessage(chrome.experimental.devtools.console.Severity.Error, obj.reason, url, objline);
 
 }
-// api.verify = function(textOrSourceCode, config, filenameOrOptions, saveState) {
+// api.v erify  = funct  ion(textOrSourceCode, config, filenameOrOptions, saveState) {
 
 
 chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(resource, content) {
   var url = resource.url;
   if (url.lastIndexOf(".js") === url.length - 3) {
-    console.log(content);
-    validate(content);
+    validate(content, url);
   }
 });
 
